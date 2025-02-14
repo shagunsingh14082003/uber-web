b73144be9ca0e85352a654233c7a1af2aa9c9624
Shagun <shagun14082003@gmail.com>
2025-02-14 23:24:07 +0530

    commit

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -25,7 +25,7 @@ module.exports.registerUser = async (req, res, next) => {
         lastname: fullname.lastname,
         email,
         password: hashedPassword
-    });
+    })
 
     const token = user.generateAuthToken();
 
